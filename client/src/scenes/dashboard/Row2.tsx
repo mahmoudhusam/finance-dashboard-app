a9d6c3cdba220feced8d0bed6327e514b0bfcb7d
Mahmoud Husam <mahayesh7@gmail.com>
2024-08-28 19:19:31 +0300

    Application completed last5

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -3,7 +3,7 @@ import DashboardBox from "../../components/DashboardBox";
 import FlexBetween from "../../components/FlexBetween";
 import { useGetKpisQuery, useGetProductsQuery } from "../../state/api";
 import { Box, Typography, useTheme } from "@mui/material";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 
 import {
   Tooltip,
@@ -134,7 +134,7 @@ const Row2 = () => {
               paddingAngle={2}
               dataKey="value"
             >
-              {pieData.map((entry, index) => (
+              {pieData.map((_, index) => (
                 <Cell key={`cell-${index}`} fill={pieColors[index]} />
               ))}
             </Pie>
